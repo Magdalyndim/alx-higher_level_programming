f63c56b99172ba294a4b52f1672344b4e106f56b
Magdalyndim <magdalyndim@gmail.com>
2022-12-05 13:21:22 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3 
+ import hidden_4 
+  
+  
+ def main(): 
+     for i in dir(hidden_4): 
+         if i[0:2] != "__": 
+             print(i) 
+  
+  
  if __name__ == "__main__": 
-     import hidden_4 
-     for name in dir(hidden_4): 
-         if name[0] != '_' and name[1] != '_': 
-             print(name)
+     main()
