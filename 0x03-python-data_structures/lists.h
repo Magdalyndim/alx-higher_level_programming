93b5110596091fea8fd30edcece6d5ae3b454e22
Magdalyndim <magdalyndim@gmail.com>
2022-12-05 22:56:07 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,6 @@
-#ifndef LISTS_H 
+#ifndef LISTS_H 
 #define LISTS_H 
   
-#include <stdio.h> 
 #include <stdlib.h> 
   
 /** 
@@ -23,6 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head); 
  
 int is_palindrome(listint_t **head); 
-int rec_palindrome(listint_t **head, listint_t *tail); 
- 
+int rec_palindrome(listint_t **head, listint_t *tail);
+
 #endif /* LISTS_H */
