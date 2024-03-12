8fddcba8532efbb4903937e19ce640d53bfef566
Magdalyndim <magdalyndim@gmail.com>
2024-03-12 05:38:57 -0700

    new commit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end='')
-
-# To print a newline after the loop
-print()
+for ch in range(97, 123):
+    print("{:c}".format(ch), end='')
